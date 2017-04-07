0ba1f3b936252f33d481753568d3727a162754e9
baiganio <big_l@abv.bg>
2017-04-07 16:04:45 +0300

    Added todays demo

---
+++ 00. (EF) Basics/Project.DB/ProjectContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.Entity;
+using Project.Models;
+
+namespace Project.DB
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext()
+            : base("name = ProjectContext")
+        {
+            
+        }
+
+        public IDbSet<User> Users { get; set; }
+        public IDbSet<Student> Students { get; set; }
+    }
+}
---
+++ 00. (EF) Basics/Project.Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Project.Common;
+
+namespace Project.Models
+{
+    public class User
+    {
+        public User(string username, string password, bool isValidated, string id = null)
+        {
+            this.Id = id ?? Guid.NewGuid().ToString();
+            this.Username = username;
+            this.Password = HashUtils.CreateHashCode(password);
+            this.IsValidated = isValidated;
+        }
+
+        [Key]
+        public string Id { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public bool IsValidated { get; private set; }
+
+        public override string ToString()
+        {
+            return $"User: {this.Username} \nwith ID: {this.Id} \nPassword: {this.Password}";
+        }
+
+    }
+}
