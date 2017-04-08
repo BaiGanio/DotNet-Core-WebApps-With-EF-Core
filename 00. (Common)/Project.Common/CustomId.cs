d473d911f84fc58dc75b7817efb8d5d574536779
baiganio <big_l@abv.bg>
2017-04-08 15:17:09 +0300

    Added interview QA

---
+++ 00. (Common)/Project.Common/CustomId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Project.Common
+{
+    public class CustomId
+    {
+        private readonly Guid _id;
+
+        public CustomId()
+        {
+            this._id = new Guid();
+        }
+
+        public CustomId(Guid guid)
+        {
+            this._id = guid;
+        }
+
+        public override string ToString()
+        {
+            return this._id.ToString();
+        }
+
+        public string ToString(string format)
+        {
+            return this._id.ToString(format);
+        }
+
+    }
+}
