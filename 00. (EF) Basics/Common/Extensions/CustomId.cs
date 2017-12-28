d62ae2e9d6b1565a3425befa581d00d820d0b1f0
BaiGanio <lubenkikov@gmail.com>
2017-12-28 12:01:31 +0200

    Something strange happen.

---
+++ 00. (EF) Basics/Common/Extensions/CustomId.cs
@@ -0,0 +1,22 @@
+﻿namespace Common.Extensions
+{
+    public class CustomId
+    {
+        private Guid id;
+
+        public CustomId()
+        {
+            this.id = Guid.NewGuid();
+        }
+
+        public CustomId(Guid guid)
+        {
+            this.id = guid;
+        }
+
+        public override string ToString()
+        {
+            return this.id.ToString();
+        }
+    }
+}
