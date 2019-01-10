913a76ca835c947c666199537a9e097d4b319ecf
(BG) Team <exceptionhelper@gmail.com>
2019-01-11 00:17:25 +0200

    Revert "Master"

---
+++ Week 1/SimUDuckApp/Interfaces/IDuck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace SimUDuckApp
+{
+    public abstract class IDuck
+    {
+        private IQuackBehavior quackBehavior;
+        private IFlyBehavior flyBehavior;
+
+        public IDuck(IQuackBehavior qb)
+        {
+            this.quackBehavior = qb;
+            //this.flyBehavior= new fly
+        }
+
+        public IDuck(IQuackBehavior qb, IFlyBehavior fb)
+        {
+            this.quackBehavior = qb;
+            this.flyBehavior = fb;
+        }
+
+        public void PerformQuack()
+        {
+            this.quackBehavior.Quack();
+        }
+
+        public void PerformFly()
+        {
+            this.flyBehavior.Fly();
+        }
+
+        //public void PerformDive()
+        //{
+        //    this.diveBehavior.Dive();
+        //}
+
+        public void SetFlyBehavior(IFlyBehavior fb)
+        {
+            this.flyBehavior = fb;
+        }
+
+
+        public void Swim()
+        {
+            Console.WriteLine("I'm swiming!");
+        }
+    }
+}
